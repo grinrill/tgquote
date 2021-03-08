553a4614f66614eb803a84717f84d3bf300516e3
grinrill <grinrill@mail.ru>
2021-03-08 13:48:37 +0000

    Allow users to add own styles instead replace itЖ

--- engine.py
+++ engine.py
@@ -17,6 +17,7 @@ class TelegramImageRenderer:
     filegetter: BaseFileGetter,
     file_format: str = 'png',
     styles: typing.Union[str, io.BytesIO, io.StringIO] = str(module/'css/light.css'),
+    append_styles: typing.Union[str, io.BytesIO, io.StringIO] = None,
     jinja2_loader: BaseLoader = None,
     jinja2_env: Environment = None,
   ):
@@ -48,7 +49,18 @@ class TelegramImageRenderer:
       self.css = styles.read()
       if isinstance(self.css, bytes):
         self.css = self.css.decode()
-
+    if append_styles:
+      if isinstance(styles, str):
+        if append_styles.endswith('.css'):
+          append_css = open(append_styles, 'r+').read()
+        else:
+          append_css = append_styles
+      else:
+        append_css = append_styles.read()
+        if isinstance(append_css, bytes):
+          append_css = append_css.decode()
+      self.css += append_css
+        
   async def render(
     self,
     messages,
@@ -56,6 +68,7 @@ class TelegramImageRenderer:
     filegetter: BaseFileGetter = None,
     file_format: str = None,
     styles: typing.Union[str, io.BytesIO, io.StringIO] = None,
+    append_styles: typing.Union[str, io.BytesIO, io.StringIO] = None,
     jinja2_loader: BaseLoader = None,
     jinja2_env: Environment = None,
   ):
@@ -89,6 +102,17 @@ class TelegramImageRenderer:
           css = css.decode()
     else:
       css = self.css
+    if append_styles:
+      if isinstance(styles, str):
+        if append_styles.endswith('.css'):
+          append_css = open(append_styles, 'r+').read()
+        else:
+          append_css = append_styles
+      else:
+        append_css = append_styles.read()
+        if isinstance(append_css, bytes):
+          append_css = append_css.decode()
+      css += append_css
 
     messages = preprocessMessagesToDict(messages)
     html = await messagesToHtml(messages, filegetter, env)
