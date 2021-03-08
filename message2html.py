e7b5398439c0f2f0fb74d2deb832e5c60e170951
grinrill <grinrill@mail.ru>
2021-03-08 09:52:08 +0000

    Added utils.base64ToUrl function Because it modifided message2html.py

--- message2html.py
+++ message2html.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 from jinja2 import Environment, FileSystemLoader
 from .filegetters import BaseFileGetter
+from . import utils
 import typing
 
 env = Environment(
@@ -10,14 +11,15 @@ env = Environment(
 
 
 async def messagesToHtml(
-  messages: typing.List[dict],
-  filegetter: BaseFileGetter,
-  env: Environment = env,
+    messages: typing.List[dict],
+    filegetter: BaseFileGetter,
+    env: Environment = env,
   ):
   template = env.get_template('index.jinja2')
 
   html = await template.render_async(
     messages = messages,
     files = filegetter,
+    utils = utils,
   )
   return html
