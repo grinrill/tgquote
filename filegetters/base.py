902488dda4a878c2315fa1ac574b73e507fc8b88
grinrill <grinrill@mail.ru>
2021-03-08 08:37:32 +0000

    Changed filegettrs api: 1. Renamed: - get_file_by_file_id to get_file - get_userpic_by_user_id to get_userpic 2. Removed: - get_file_by_message - get_userpic_by_message 3. Added: - get_thumb - get_document get_thumb and get_document returns telegram file object, not file.

--- filegetters/base.py
+++ filegetters/base.py
@@ -1,12 +1,48 @@
 class BaseFileGetter:
-  async def get_file_by_file_id(self, file_id: str):
+  async def get_file(self, file_id: str):
     raise NotImplementedError()
 
-  async def get_file_by_message(self, message: dict):
+  async def get_userpic(self, user_id: int):
     raise NotImplementedError()
 
-  async def get_userpic_by_user_id(self, user_id: int):
-    raise NotImplementedError()
+  async def get_thumb(self, message):
+    """
+    Тупой алгоритм,
+    который рекурсивно с конца ищет поле "thumb"
+    и если находит, возвращает его
+    """
+    if isinstance(message, list):
+      values = list
+    elif isinstance(message, dict):
+      values = list(message.values())
+    else: 
+      return
+    values.reverse()
 
-  async def get_userpic_by_message(self, message: dict):
-    raise NotImplementedError()
+    for i in values:
+      if isinstance(i, dict):
+        if 'thumb' in i:
+          return i['thumb']
+      if result := await self.get_thumb(i):
+        return result
+
+  async def get_document(self, message):
+    """
+    Тупой алгоритм,
+    который рекурсивно с конца ищет поле "file_id"
+    и если находит, возвращает его родителя
+    """
+    if isinstance(message, list):
+      values = list
+    elif isinstance(message, dict):
+      values = list(message.values())
+    else: 
+      return
+    values.reverse()
+
+    for i in values:
+      if isinstance(i, dict):
+        if 'file_id' in i:
+          return i
+      if result := await self.get_document(i):
+        return result
