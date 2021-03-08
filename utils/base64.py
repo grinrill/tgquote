bc8917e074c370b40dfeb133dbb449ac6bd392f2
grinrill <grinrill@mail.ru>
2021-03-08 10:05:30 +0000

    fix error in base64ToUrl coverter

--- utils/base64.py
+++ utils/base64.py
@@ -4,4 +4,4 @@ def base64ToUrl(bytesio, mime_type):
   encoded = base64.b64encode(
     bytesio.read()
   )
-  return f'data:{mime_type};base64, {encoded}'
+  return f'data:{mime_type};base64, {encoded.decode()}'
