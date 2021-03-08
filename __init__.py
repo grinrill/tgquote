64a719b5d832cd7bdd9b0bc7a734d4518397806d
grinrill <grinrill@mail.ru>
2021-03-08 13:18:09 +0000

    Edit template

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@ from .message2html import env, messagesToHtml
 from .preprocessing import preprocessMessagesToDict
 from .engine import TelegramImageRenderer
 
+from . import utils
 from . import renderers
 from . import filegetters
